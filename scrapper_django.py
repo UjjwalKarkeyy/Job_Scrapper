205d3a7c3f2815d99b3b20a1b58b94a44a340c46
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2025-04-05 21:07:16 +0545

    starting-django-scrapping-next

---
+++ scrapper_django.py
@@ -0,0 +1,7 @@
+'''
+    Internsathi: https://internsathi.com/internships?sort=NEWEST
+    LinkedIn: https://www.linkedin.com/jobs/search?position=1&pageNum=0
+    Intern Nepal: https://internepal.com.np/vacancy-list?type=internship&keyword=Intern
+    Internship in Nepal: https://internshipinnepal.com/jobs-list-v1/?filter-title=Intern&filter-distance=50&filter-type=127&filter-date-posted=all
+'''
+
